ab28c69394a8d3c48b2efa6748eb6b70047a5825
Alexandre Marcos <alexandremarcos94100@gmail.com>
2022-03-31 10:58:58 +0200

    CLIENT - Adding index.mjs + Create getProducts and iife function

---
+++ client/client.mjs
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const iife = () => {
+    getProducts();
+    await getProductsAsync();
+    addProduct("Melon", 15);
+}
+
+const getProducts = () => {
+    axios.get("localhost:3000/products")
+    .then((datas => {
+        console.log(JSON.parse(datas));
+    }))
+    .catch((err => {
+        console.log(err);
+    }))
+}
+
+const getProductsAsync = async () => {
+    axios.get("localhost:3000/products")
+}
+
+const addProduct = (name, quantity) => {
+    axios.post("localhost:3000/products")
+}
