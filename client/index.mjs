efbdfcb4bf0492fdb33b1c8d25d9ffd10b9c76a4
Alexandre Marcos <alexandremarcos94100@gmail.com>
2022-03-31 12:07:44 +0200

    CLIENT - Update addProducts, all functions have been tested

---
+++ client/index.mjs
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const getProducts = () => {
+    axios.get("http://localhost:3000/products")
+    .then((response) => {
+        console.log(response.data);
+    });
+}
+
+const getProductsAsync = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/products');
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+const addProduct = (name, quantity) => {
+    axios.post('http://localhost:3000/products', {
+        name: name,
+        quantity: quantity
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+}
+
+(async () => {
+    getProducts();
+    const res = await getProductsAsync();
+    addProduct("Chocolat", 250);
+  })();
+
