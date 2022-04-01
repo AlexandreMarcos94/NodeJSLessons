c91549af652fb9ac74bd78f81ebd8c3dc547380f
Alexandre Marcos <alexandremarcos94100@gmail.com>
2022-04-01 10:12:31 +0200

    Adding Jest and @types/jest to perform unit test + Creating test directory + calc.js to perform test

---
+++ test/calc.js
@@ -0,0 +1,15 @@
+
+const Add = (a, b) => {
+    let res = a + b;
+    return res
+}
+
+const Substract = (a, b) => {
+    let res = a - b;
+    return res;
+}
+
+const Multiply = (a, b) => {
+    let res = a * b;
+    return res;
+}
