34f5993a821ce0cfc3dd23229dc5d78c82771ee5
Alexandre Marcos <alexandremarcos94100@gmail.com>
2022-04-01 12:10:22 +0200

    Adding docs folder + create index.mjs + install swagger

---
+++ docs/index.mjs
@@ -0,0 +1,13 @@
+export default {
+    openapi: "3.0.1",
+    info: {
+        version: "1.0.0",
+        title: "NodeJS API",
+        description: "NodeJS API REST ESGI",
+        contact: {
+            name: "ESGI Contact",
+            email: "info@esgi.fr",
+            url: "https://esgi.fr"
+        }
+    }
+};
--- server/index.mjs
+++ server/index.mjs
@@ -2,6 +2,8 @@ import router from "./routes.mjs"
 import express from "express"
 import bodyparser from "body-parser"
 import morgan from "morgan"
+import swaggerUI from "swagger-ui-express"
+import docs from "../docs/index.mjs"
 
 const port = process.env.PORT || 3000;
 const app = express()
@@ -10,6 +12,7 @@ app.use(bodyparser.json())
 app.use(morgan('tiny'))
 
 app.use(router)
+app.use("/swagger", swaggerUI.serve, swaggerUI.setup(docs))
 
 app.listen(port, () => {
   console.log(`server listen at ${port}`)
