185e9272817959b2ede57f57cbc8561d29b54136
Alexandre Marcos <alexandremarcos94100@gmail.com>
2022-04-01 11:50:51 +0200

    Comment code (index.mjs) and update calc.test.js

--- client/index.mjs
+++ client/index.mjs
@@ -1,5 +1,8 @@
 import axios from "axios";
-
+/**
+ * Recupère l'ensemble des produits
+ * @returns json
+ */
 const getProducts = () => {
     axios.get("http://localhost:3000/products")
     .then((response) => {
@@ -7,6 +10,11 @@ const getProducts = () => {
     });
 }
 
+
+/**
+ * Recupère l'ensemble des produits de manière asynchrone
+ * @returns json
+ */
 const getProductsAsync = async () => {
     try {
       const response = await axios.get('http://localhost:3000/products');
@@ -16,6 +24,13 @@ const getProductsAsync = async () => {
     }
   }
 
+
+/**
+ * Ajoute un produit dans le fichier products.json
+ * @param {string} name
+ * @param {Integer} quantity
+ * @returns json
+ */
 const addProduct = (name, quantity) => {
     axios.post('http://localhost:3000/products', {
         name,
@@ -29,6 +44,9 @@ const addProduct = (name, quantity) => {
       });
 }
 
+/**
+ * Fonction IIFE exécute les fonction
+ */
 (async () => {
     getProducts();
     const res = await getProductsAsync();
--- test/calc.test.js
+++ test/calc.test.js
@@ -1,7 +1,17 @@
 import { Add, Substract, Multiply } from "./calc.mjs";
+import { getAll } from "../server/products.mjs";
+
+// const pdt = [{
+//     "name": "Poire",
+//     "quantity": 20
+// }]
 
 test("Perform test to know if 2 + 2 = 4", () => {
     expect(Add(2, 2)).toEqual(4);
+})
+
+test("Perform test to know if 1 + 2 = 4", () => {
+    expect(Add(1, 2)).toEqual(4);
 })
 
 test("Perform test to know if 3 - 2 = 1", () => {
@@ -11,3 +21,9 @@ test("Perform test to know if 3 - 2 = 1", () => {
 test("Perform test to know if 3 * 2 = 6", () => {
     expect(Multiply(3, 2)).toEqual(6);
 })
+
+// Utilisation de MOCK pour les tests sur fichier json 
+
+// test("Perform test to know if getAll() is ok", () => {
+//     expect(getAll()).toMatchObject(pdt);
+// })
